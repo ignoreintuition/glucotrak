f503f0a3c72a0adf66c6a707e0e840607ecc3287
Brian Greig <brian@resurgencewebdesign.com>
2016-08-26 22:55:07 -0400

    Updated dynamic port

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ MongoClient.connect(
 	(err, database) => {
 	if (err) return console.log(err)
 	db = database
-	app.listen(3000, () => {
-		console.log('listening on 3000')
+	app.listen(process.env.PORT || 3000, () => {
+		console.log('listening')
 	})
 })
 
