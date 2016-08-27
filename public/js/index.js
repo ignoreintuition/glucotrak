2258ad92f1b2be5aac9009476ac38cfea2d6da45
Brian Greig <brian@resurgencewebdesign.com>
2016-08-26 22:15:33 -0400

    Added DB read

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,18 @@
+var glucotrak = glucotrak || {}
+
+glucotrak.log = [];
+
+$.getJSON("/req", function(result){
+	$.each(result, function(i, field){
+		glucotrak.log.push(field);
+	});
+});
+
 new Vue({
 	el: '#app',
 	data: {
 		newBloodSugar: null,
-		log: [],
+		log: glucotrak.log,
 		avg: 0
 	},
 	methods: {
@@ -13,7 +23,10 @@ new Vue({
 					value: val,
 					date: Date(Date.now()).toLocaleString('en-US')
 				});
-				$.post('/resp', { bloodSugar: val } );
+				$.post('/resp', {
+					value: val,
+					date: Date(Date.now()).toLocaleString('en-US')
+				});
 				var sum = 0;
 				for( var i = 0; i < this.log.length; i++ ){
 					sum += parseInt( this.log[i].value, 10 ); 
--- server.js
+++ server.js
@@ -10,28 +10,40 @@ const config		= require('./config.js');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html')
-})
-
-app.post('/resp', (req, res) => {
-	db.collection('glucotrak').save(req.body, (err, result) => {
-		if (err) return console.log(err)
-		console.log('saved to database')
-		res.redirect('/')
-	})
-})
-
-
 var db
-
-MongoClient.connect('mongodb://'+config.mongo.user+':'+config.mongo.password+'@ds013966.mlab.com:13966/rwd-test', (err, database) => {
+MongoClient.connect(
+	'mongodb://' +
+		config.mongo.user + ':' +
+		config.mongo.password +
+		config.mongo.database, 
+	(err, database) => {
 	if (err) return console.log(err)
 	db = database
 	app.listen(3000, () => {
 		console.log('listening on 3000')
 	})
-
 })
+
+app.get('/req', (req, res) => {
+	var cursor = db.collection('glucotrak').find().toArray(function(err, results) {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify(results, null, 3));
+	})
+	//res.redirect('/');
+})
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+})
+
+app.post('/resp', (req, res) => {
+	db.collection('glucotrak').save(req.body, (err, result) => {
+		if (err) return console.log(err)
+			console.log('saved to database')
+		res.redirect('/')
+	})
+})
+
+
 
 
