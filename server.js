2dcd7b8aacbccbaa3982876aed7097715cc1931b
Brian Greig <brian@resurgencewebdesign.com>
2016-08-28 21:44:49 -0400

    added authentication

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 // server.js
 
 const express 			= require('express');
+const session 			= require('express-session');
 const bodyParser 		= require('body-parser');
 const app 				= express();
 const path 				= require('path');
 const MongoClient		= require('mongodb').MongoClient;
 const passport			= require('passport')
 const LocalStrategy 	= require('passport-local').Strategy;
+const jwt 				= require('jsonwebtoken');
+const cookieParser		= require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: false }
+}))
 app.use(passport.initialize());
+app.use(passport.session());
 
 var db
 MongoClient.connect(
@@ -24,22 +35,25 @@ MongoClient.connect(
 		})
 	})
 
-app.get('/req', (req, res) => {
-	var cursor = db.collection('glucotrak').find().toArray(function(err, results) {
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/req', function(req, res)  {
+	passport.authenticate('local');
+	var cursor = db.collection('glucotrak').find({"userid":req.user._id}).toArray(function(err, results) {
 		res.setHeader('Content-Type', 'application/json');
 		res.send(JSON.stringify(results, null, 3));
 	})
-})
-
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/index.html');
 })
 
 app.get('/login', (req, res) => {
 	res.sendFile(__dirname + '/public/login.html');
 })
 
-app.post('/resp', (req, res) => {
+app.post('/resp', (req, res ) => {
+	passport.authenticate('local');
+	req.body.userid = req.user._id;
 	db.collection('glucotrak').save(req.body, (err, result) => {
 		if (err) return console.log(err)
 			console.log('saved to database')
@@ -53,19 +67,18 @@ passport.use(new LocalStrategy(
 			if (err) {
 				console.log('err');
 				return done(err);}
-			if(!user){
-				console.log('!user');
-				return done(null, false, {message: 'incorrect username'});
-			}
-			if(user.password != password){
-				console.log('!password')
-				return done(null, false, {message: 'incorrect password'})
-			}
-			console.log('logged in')
-			return done(null, user);
-		});
-	}
-	)
+				if(!user){
+					console.log('!user');
+					return done(null, false, {message: 'incorrect username'});
+				}
+				if(user.password != password){
+					console.log('!password')
+					return done(null, false, {message: 'incorrect password'})
+				}
+				console.log('logged in');
+				return done(null, user);
+			});
+	})
 );
 
 app.post('/login', 
@@ -73,16 +86,13 @@ app.post('/login',
 		successRedirect: '/', 
 		failureRedirect: '/login'
 	})
-);
+	);
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
+	done(null, user);
 });
-
-
-
 
